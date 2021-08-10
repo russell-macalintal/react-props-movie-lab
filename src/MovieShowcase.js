b43d877a09f864093122e2ad8c7da2594eba9ea7
russell-macalintal <russell-macalintal@gmail.com>
2021-08-10 02:36:12 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,14 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map( info => <MovieCard title={info["title"]} IMDBRating={info["IMDBRating"]} genres={info["genres"]} poster={info["poster"]} />);
+
+    // Alternate code to return a correct array 
+    // let movieCardArr = [];
+    // for( let i = 0; i < movieData.length; i++ ) {
+    //   movieCardArr.push(<MovieCard title={movieData[i]["title"]} IMDBRating={movieData[i]["IMDBRating"]} genres={movieData[i]["genres"]} poster={movieData[i]["poster"]} />);
+    // }
+    // return movieCardArr;
   }
 
   render() {
